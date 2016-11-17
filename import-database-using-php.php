33dc6ea080f6b74f3b0b3a72ec49d71518750350
Arvindsingh Purohit <purohitarvind77@yahoo.com>
2016-11-17 22:10:16 +0530

    Add files via upload

---
+++ import-database-using-php.php
@@ -0,0 +1,16 @@
+<?php
+	$filename= 'abc.sql';
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'technogenous';
+    $con=mysqli_connect($hostname,$username,$password);
+    mysqli_query($con,"DROP DATABASE IF EXISTS ".$dbname) or die(mysqli_error($con));
+    mysqli_query($con,"CREATE DATABASE "..$dbname) or die(mysqli_error($con));
+    mysqli_select_db($con,$dbname) or die(mysqli_error($con));
+    $sqlfile = file_get_contents($filename);
+    $sqlstrings=explode(";",$sqlfile);
+	foreach($sqlstrings as $line) {
+	   	mysqli_query($con,$line);
+	}
+?>
